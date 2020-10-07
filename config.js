4de9e4409a562f3c06fd60694021012ca7cceb3f
David Ma <david.ma@petermac.org>
2020-10-07 19:03:34 +1100

    Remove unused code, split cas13 project into it's own repository

--- config.js
+++ config.js
@@ -14,31 +14,11 @@ const mail_auth = config.mail_auth;
 
 exports.config = {
     redirects: {
-        "/help": "https://vm-115-146-91-157.melbourne.rc.nectar.org.au/confluence/display/PVS/How-to+articles",
+        "/cas13": "//cas13.david-ma.net",
+        "/help": "https://atlassian.petermac.org.au/confluence/display/PVS/How-to+articles",
         "/dropbox": "https://www.dropbox.com/sh/95s0p83vcwnuj5h/AABi4YHSnB40SQa2XJfRXOpfa?dl=0/",
-        "/DEMONSTRATORS": "http://115.146.84.92:8123/PathOS/",
-        "/A1": "http://115.146.84.87:8123/PathOS/",
-        "/A2": "http://115.146.84.98:8123/PathOS/",
-        "/A3": "http://115.146.84.80:8123/PathOS/",
-        "/A4": "http://115.146.84.99:8123/PathOS/",
-        "/A5": "http://115.146.84.49:8123/PathOS/",
-        "/A6": "http://115.146.84.96:8123/PathOS/",
-        "/A7": "http://115.146.84.93:8123/PathOS/",
-        "/A8": "http://115.146.84.100:8123/PathOS/",
-        "/B1": "http://115.146.84.90:8123/PathOS/",
-        "/B2": "http://115.146.84.104:8123/PathOS/",
-        "/B3": "http://115.146.84.97:8123/PathOS/",
-        "/B4": "http://115.146.84.101:8123/PathOS/",
-        "/B5": "http://115.146.84.108:8123/PathOS/",
-        "/B6": "http://115.146.84.109:8123/PathOS/",
-        "/B7": "http://115.146.84.107:8123/PathOS/",
-        "/B8": "http://115.146.84.105:8123/PathOS/",
-        "/OBSERVERS": "http://115.146.84.91:8123/PathOS/",
-        "/bbq": "https://doodle.com/poll/c2iqixwmra48vbiw",
-        "/BBQ": "https://doodle.com/poll/c2iqixwmra48vbiw",
         "/Oberyn": "https://docs.google.com/presentation/d/e/2PACX-1vRbVjtvKU_jFcqWVnYxXQrHdB30Y2Q0oxp1IGDgBu4X_9I3Gm3Q2mTU2SAKl4wcp6A7OT7lIwQgmV1t/pub?start=false&loop=false&delayms=3000",
         "/oberyn": "https://docs.google.com/presentation/d/e/2PACX-1vRbVjtvKU_jFcqWVnYxXQrHdB30Y2Q0oxp1IGDgBu4X_9I3Gm3Q2mTU2SAKl4wcp6A7OT7lIwQgmV1t/pub?start=false&loop=false&delayms=3000"
-
     },
     "data": true,
     "domains": ["www.pathos.co", "pathos.co"],
@@ -57,15 +37,6 @@ exports.config = {
         }
     },
     controllers: {
-        cas13: function(router) {
-            router.readTemplate('cas13.mustache', 'd3_inner', views => {
-                console.log("hey");
-                var data = {};
-
-                var output = mustache.render(views.template, data, views);
-                router.res.end(output);
-            });
-        },
         kras: function(router) {
             router.readTemplate('wrapper.mustache', 'd3_inner', views => {
                 console.log(router.path);
@@ -130,25 +101,6 @@ exports.config = {
                     var output = mustache.render(views.template, data, views);
                     router.res.end(output);
                 }
-            })
-        },
-        old_kras: function (router) {
-            // console.log(Object.keys(router.views));
-            router.readAllViews(function(views){
-                const data = {
-                    images: [
-                        {image: '/images/a.jpg'}
-                        // {image: '/images/prob.jpeg'}
-                    ],
-                    foo: [{
-                        abc: 123
-                    }]
-                };
-
-                // console.log(Object.keys(views));
-
-                var output = mustache.render(views.kras, data, views);
-                router.res.end(output);
             })
         },
         mail: function (router) {
