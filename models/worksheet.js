15ecc5a06629bc500a196fdc72ec8024af1a85eb
David Ma <github@david-ma.net>
2020-10-26 16:30:47 +1100

    Add typescript for models

--- models/worksheet.js
+++ models/worksheet.js
@@ -1,15 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Worksheet = sequelize.define('Worksheet', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    doctor: DataTypes.STRING,
-    hash: DataTypes.STRING,
-    data: DataTypes.TEXT
-  }, {});
-  Worksheet.associate = function(models) {
-    // associations can be defined here
-  };
-  return Worksheet;
+    const Worksheet = sequelize.define('Worksheet', {
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        doctor: DataTypes.STRING,
+        hash: DataTypes.STRING,
+        data: DataTypes.TEXT
+    }, {});
+    Worksheet.associate = function (models) {
+        // associations can be defined here
+    };
+    return Worksheet;
 };
